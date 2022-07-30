f507a4359770ef414b1c4c56a7f2fd2d46409669
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-07-30 21:34:05 +0200

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const galleryImages = galleryItems
+    .map((image) => {
+        const item = `<div class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+        <img
+        class="gallery__image"
+        src="${image.preview}"
+        data-source=${image.original}"
+        alt="${image.description}"
+        />
+        </a>
+        </div>`
+        return item;
+    })
+    .join(" ");
+
+gallery.insertAdjacentHTML('afterbegin', galleryImages);
+
+new SimpleLightbox('.gallery a', {captionDelay: 250});
 
 console.log(galleryItems);
