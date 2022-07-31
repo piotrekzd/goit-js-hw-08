425e7346eda1dbf380abf19da6d9cf4f27da5bcc
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-07-31 12:59:11 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import { throttle } from "lodash";
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const message = document.querySelector('textarea');
+const localStorageKey = 'feedback-form-state';
+
+form.addEventListener('input', throttle((e) => {
+    const inputData = { email: form.email.value, message: form.message.value };
+    localStorage.setItem(localStorageKey, JSON.stringify(inputData));
+}, 500));
+
+const storageData = localStorage.getItem(localStorageKey);
+const parseData = JSON.parse(storageData);
+
+form.addEventListener('input', (e) => {
+    e.preventDefault();
+    const { elements: { email, message } } = e.currentTarget;
+    console.log({ email: email.value, message: message.value });
+});
+
+const resetForm = () => {
+    if (parseData !== '') {
+        input.value = parseData.email;
+        message.value = parseData.message;
+    }
+}
+
+resetForm();
